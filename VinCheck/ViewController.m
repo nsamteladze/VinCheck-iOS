2b6008754cf0bff5c2eb7975c0ea7a3508f8bbc0
nsamteladze <nikolai.samteladze@c1bank.com>
2014-11-27 00:35:16 -0500

    Initial Commit

---
+++ VinCheck/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VinCheck
+//
+//  Created by Nikolai Samteladze on 11/27/14.
+//  Copyright (c) 2014 Cocos Games. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ VinCheck/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VinCheck
+//
+//  Created by Nikolai Samteladze on 11/27/14.
+//  Copyright (c) 2014 Cocos Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
